dd74c165e33770152618e04067b9c840f5fb131f
username <useremail>
2024-10-08 19:38:26 -0700

    Update

--- GuessingGame.py
+++ GuessingGame.py
@@ -8,19 +8,23 @@ import random
 print("Time to guess the number I'm thinking of!")
 
 #Part 2
+
 numberLimit = int(input("\nEnter the limit:     "))
 print(f"I am thinking of a number between 1 and {numberLimit} ")
-lowerEnd = 1
-upperEnd = {numberLimit}
-supriseNum = random.randint(lowerEnd, upperEnd)
+supriseNum = random.randint(1,numberLimit)
+print(f"{supriseNum}")
+chosenNum = int(input("Guess a number within specified range?"))
+while chosenNum != supriseNum:
 
-chosenNum = int(input(f"\nGuess a number within specified range?     )
-if {chosenNum} == {supriseNum}:
-    print(f"\nNice job you guessed correctly!")
+    
+    
+    if chosenNum < supriseNum:
+        print("Your guess is too low, try again")
 
-elif {chosenNum} > {supriseNum}:
-    print(f"\nYour guess is too high, try again."
+    elif chosenNum > supriseNum:
+        print("Your guess is too high, try again.")
 
-else:
-    print(f"\nYour guess is too low, try again."
+    else:
+        print("Your guess is correct. Nice job!")
+
 
