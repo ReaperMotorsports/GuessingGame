58f26b0e549dc7057d66b07e38901dd2907e4c9d
username <useremail>
2024-10-09 20:17:20 -0700

    FINAL final draft

--- GuessingGame.py
+++ GuessingGame.py
@@ -8,28 +8,29 @@ import random
 print("Time to guess the number I'm thinking of!")
 
 #Part 2
+
 keepPlaying = "y"
-while keepPlaying.lower() == "y":
-    
+while keepPlaying == "y":
+    attempts = 0
     numberLimit = int(input("\nEnter the limit:     "))
     print(f"I am thinking of a number between 1 and {numberLimit} ")
     supriseNum = random.randint(1,numberLimit)
     chosenNum = int(input("Guess a number within specified range:     "))
+    attempts += 1
     while chosenNum != supriseNum:
-
-    
     
         if chosenNum < supriseNum:
             print(f"Your guess is too low, try again")
             chosenNum = int(input("Your guess:     "))
-
+            attempts += 1
         elif chosenNum > supriseNum:
             print(f"Your guess is too high, try again")
             chosenNum = int(input("Your guess:    "))
-
+            attempts += 1
         else:
             break
- 
     print("Your guess is correct. Nice job!")
+    print(f"\nYou guessed it in {attempts} attempts.")
     keepPlaying = input(f"\nWould you like to play again? (y/n):     ")
+
 print(f"\nGood Bye.")
